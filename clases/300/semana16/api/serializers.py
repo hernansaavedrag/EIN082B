5a13b7f1a61ddddf6d86368d76c13923cb298a04
hernansaavedrag <hernan.saavedra@gmail.com>
2024-04-22 09:42:11 -0400

    Init 2024-1

--- clases/300/semana16/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from miapp.models import Carrera
-
-class CarreraSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Carrera
-        fields = '__all__'
-        #fields = ['nombre','duracion']
-        #exclude = ['codigo']
--- clases/301/semana16/rest/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import CarreraSerializer
-from miapp.models import Carrera
-
-class CarreraViewSet(viewsets.ModelViewSet):
-    queryset = Carrera.objects.all()
-    serializer_class = CarreraSerializer
