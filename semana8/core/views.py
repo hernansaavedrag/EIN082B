2c5965d08fd4243fd3f4ae2b4ea79a93b46ed941
pgaticac <pamela.gatica@usm.cl>
2024-04-29 10:57:55 -0400

    Templates + Static

---
+++ semana8/core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    #return HttpResponse("Hola Mundo")
+    return render(request,'core/home.html')
+
+def carreras(request):
+   
+    return render(request,'core/carreras.html')
+
+def docentes(request):
+
+    return render(request,'core/docentes.html')
